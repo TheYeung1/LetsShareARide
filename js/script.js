fb91d337a881387ef6927e149e0f2122b6fab465
Winston Yeung <yeung.winston@gmail.com>
2014-01-25 05:18:42 -0500

    submit new driver yaaa

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
         $('#availableDrivers').empty();
         var Drivers = Parse.Object.extend('Driver');
         var query = Parse.Query(Drivers);
+        query.equalTo('eventID', $('#riderEventSelect').val());
         query.find({
           success: function(results){
             for (var i = 0; i < results.length; i++) { 
@@ -17,4 +18,39 @@ $(document).ready(function(){
           }
         })
     });
+    $('#driverSignUpSubmit').on('click', function(){
+        var fbEventID = $('#driverSignUpForm select.EventsDropdownForm').val();
+        var numSeats = $('#driverSignUpForm input').val();
+        var Drivers = Parse.Object.extend('Driver');
+        var query = Parse.Query(Drivers);
+        query.equalTo('eventID', fbEventID);
+        query.equalTo('DriverID', Parse.Users.current());
+        // check if this user already signed up for this ride, if they did then ya
+        query.first({
+          success: function(object){
+              if (object){
+                console.log('user already signed up for this');
+                // add a prompr for user
+              } else {
+                var newRide = new Drivers();
+                newRide.set('DriverID', Parse.Users.current());
+                newRide.set('eventID', fbEventID);
+                newRide.set('Seats', numSeats);
+                newRide.save(null, {
+                  success: function(response){
+                    //do stuff
+                  },
+                  error: function(error){
+                    // do stuff
+                  }
+                });
+              }
+          }
+        })
+    })
 });
+
+function getEventObject(eventID){
+  var Events = Parse.Object.extend('Event');
+  var query = Parse.Query(Events);
+}
