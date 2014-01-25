f31b102953c6cb06b5c2da2fc5d0568f5fa0bb95
Winston Yeung <yeung.winston@gmail.com>
2014-01-24 22:24:45 -0500

    added debug statements to oauth change to check behaviour

--- js/fb.js
+++ js/fb.js
@@ -18,13 +18,17 @@
   // whenever someone who was previously logged out tries to log in again, the correct case below 
   // will be handled. 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
+      
+    console.log("YOLO!!");
     // Here we specify what we do with the response anytime this event occurs. 
     if (response.status === 'connected') {
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
+      console.log("connected yo");
       testAPI();
     } else if (response.status === 'not_authorized') {
+      console.log("not authorized yo");
       // In this case, the person is logged into Facebook, but not into the app, so we call
       // FB.login() to prompt them to do so. 
       // In real-life usage, you wouldn't want to immediately prompt someone to login 
@@ -47,6 +51,7 @@
         console.log("not authorized");
         testAPI();
     } else {
+        console.log("not logged into fb yo");
       // In this case, the person is not logged into Facebook, so we call the login() 
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
